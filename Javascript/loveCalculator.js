b7ce6e3c631c3215255719d9a250d43067a7eed1
Isaac Xavier Desouza <66382402+isaacdes@users.noreply.github.com>
2022-02-14 23:21:30 +0530

    lovecalculator

---
+++ Javascript/loveCalculator.js
@@ -0,0 +1,6 @@
+//  prompt("What is your name? ");
+//  prompt("What is their name? ");
+
+var loveScore = Math.random() * 100;
+loveScore = Math.floor(loveScore) + 1; //increment 1 as sometimes it goes to 0 and also dosent touch 100
+console.log("Love Score is: " +  loveScore);
